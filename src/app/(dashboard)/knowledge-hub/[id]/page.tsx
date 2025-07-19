f17da6aea9047d47e94a26393fbf7d72ab6030e8
aseelflihan <aseelflihan@gmail.com>
2025-07-20 00:47:58 +0800

    fix

--- src/app/(dashboard)/knowledge-hub/[id]/page.tsx
+++ src/app/(dashboard)/knowledge-hub/[id]/page.tsx
@@ -1,14 +1,10 @@
 import KnowledgeDetail from "../../../../components/knowledge-hub/KnowledgeDetail";
 
-type Props = {
+export default async function KnowledgeItemPage({
+  params,
+}: {
   params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export default function KnowledgeItemPage({ params }: Props) {
-  if (!params.id) {
-    return <div>Error: ID not found</div>;
-  }
+}) {
   return (
     <div className="p-4 sm:p-6 md:p-8">
       <KnowledgeDetail id={params.id} />
